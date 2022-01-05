de2b4a2192a588f3f8dbfd17919a43ef3fff1479
Rurek5000 <56135268+Rurek5000@users.noreply.github.com>
2022-01-05 18:39:04 +0100

    Comment view created and styled

---
+++ resources/views/partials/comment.blade.php
@@ -0,0 +1,21 @@
+<div class="comment">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="comment__wrapper">
+                    <div class="comment-author d-flex align-items-center">
+                        <div class="comment-author__image-wrapper">
+                            <img src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/0032047ebe6bad7a6914fabddce8e054fa651fb8_full.jpg" alt="" class="comment-author__image w-100 h-100 object-fit-cover">
+                        </div>
+                        <div class="comment-author__data">
+                            <div class="comment-author__name">Rosy Rose</div>
+                        </div>
+                    </div>
+                    <div class="comment-content">
+                        <div class="comment-content__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur animi fugiat explicabo modi expedita, vel natus minus voluptas eos earum, tempore, ipsum dolorem accusantium veniam iusto quibusdam veritatis nulla nostrum. Aspernatur animi fugiat explicabo modi expedita, vel natus minus voluptas eos earum, tempore, ipsum dolorem accusantium veniam iusto quibusdam veritatis nulla nostrum.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
