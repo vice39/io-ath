46ce5e49acb93d2686f4383137b44458510fd53a
Rurek5000 <56135268+Rurek5000@users.noreply.github.com>
2022-01-12 17:29:25 +0100

    Main bar styling

---
+++ resources/views/partials/main-bar.blade.php
@@ -0,0 +1,21 @@
+<div class="main-bar">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-start align-items-center flex-shrink-1">
+                <div class="main-bar__image-wrapper">
+                    <img src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/0032047ebe6bad7a6914fabddce8e054fa651fb8_full.jpg" alt="" class="meeting-author__image w-100 h-100 object-fit-cover">
+                </div>
+                <input type="text" class="main-bar__input flex-grow-1" placeholder="Time to meet">
+            </div>
+            <div class="col-12">
+                <div class="main-bar__category-wrapper d-flex justify-content-start align-items-center">
+                    <div class="main-bar__single-category">Love</div>
+                    <div class="main-bar__single-category">Travel</div>
+                    <div class="main-bar__single-category">Food</div>
+                    <div class="main-bar__single-category">Arts</div>
+                    <div class="main-bar__filter"><i class="fas fa-filter"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
