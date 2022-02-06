226c458a4d45b9bd0a953e676ca7d2ac71e9a0f1
Michał Wybierała <mw056811@student.ath.edu.pl>
2022-02-06 13:28:31 +0100

    Connect meetings to the index page

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\MeetingController::class, 'index']);
 
 Route::get('/my-profile', [MyProfileController::class, 'index']);
 
