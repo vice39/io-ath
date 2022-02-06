7074bcc4806a0e11486cebe03c255b4f8378d15e
Michał Wybierała <mw056811@student.ath.edu.pl>
2022-02-06 13:27:46 +0100

    Added comments to the meetings

--- resources/views/meeting.blade.php
+++ resources/views/meeting.blade.php
@@ -9,5 +9,27 @@
     ])
     @endcomponent
 
+    @foreach($meeting->comments as $comment)
+        @include('partials.comment', ['comment' => $comment])
+    @endforeach
+
+    <div class="comment">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h4>Dodaj komentarz</h4>
+                    <form method="post" action="{{ route('meeting.comments.store', [ 'meeting' => $meeting ]) }}">
+                        @csrf
+
+                        <textarea name="content">
+                        </textarea>
+
+                        <input type="submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 @endsection
