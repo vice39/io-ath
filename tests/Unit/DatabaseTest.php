e9c14bcf198ecde5014f91b3c947ac1021a779dc
daniel <nic41@vp.pl>
2022-02-06 12:42:52 +0100

    Add Db tables unit test

--- tests/Unit/DatabaseTest.php
+++ tests/Unit/DatabaseTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use App\Models\Meeting;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class DatabaseTest extends TestCase
@@ -39,5 +40,15 @@ class DatabaseTest extends TestCase
         self::assertNotContains($author->id, $ids);
         self::assertEquals($author->id, $meeting->author->id);
         self::assertEquals($author->createdMeetings->first()->id, $meeting->id);
+    }
+
+    public function test_db_should_contains_required_tables() {
+        self::assertTrue(Schema::hasTable('users'), 'users not found');
+        self::assertTrue(Schema::hasTable('categories'), 'categories not found');
+        self::assertTrue(Schema::hasTable('category_meeting'), 'category_meeting not found');
+        self::assertTrue(Schema::hasTable('comments'), 'comments not found');
+        self::assertTrue(Schema::hasTable('meetings'), 'meetings not found');
+        self::assertTrue(Schema::hasTable('meeting_comment'), 'meeting_comment not found');
+        self::assertTrue(Schema::hasTable('meeting_user'), 'meeting user not found');
     }
 }
