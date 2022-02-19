e7585f7120d8b60bc856793b22355790b4517e77
daniel <nic41@vp.pl>
2022-02-19 19:46:54 +0100

    Add map to meeting view

--- resources/views/components/meeting.blade.php
+++ resources/views/components/meeting.blade.php
@@ -1,3 +1,15 @@
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+      integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+      crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+        integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+        crossorigin=""></script>
+<style>
+    #map {
+        height: 250px;
+        margin: 20px 0;
+    }
+</style>
 <div class="meeting">
     <div class="container">
         <div class="meeting__wrapper">
@@ -47,3 +59,16 @@
         </div>
     </div>
 </div>
+<div id="map"></div>
+<script>
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGFuaWVsNTEyIiwiYSI6ImNrenU2dzdmODRzdDMyd25yODNxdDkwdjYifQ.Ov_YAbWRqdh7XKfJ6GbHNw', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'your.mapbox.access.token'
+    }).addTo(map);
+</script>
