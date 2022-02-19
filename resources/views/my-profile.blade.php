6f44b61c85b75ff819d2d8fc730fdf2229e0ed12
Michał Wybierała <mw056811@student.ath.edu.pl>
2022-02-19 11:09:53 +0100

    Added logout to my profile

--- resources/views/my-profile.blade.php
+++ resources/views/my-profile.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-My Profile
+    <div class="container d-flex flex-column align-items-center">
+        <h1>Mój profil</h1>
+
+        <div>Jesteś zalogowany jako {{ auth()->user()->name }}</div>
+
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+
+            <button type="submit">Wyloguj się</button>
+        </form>
+    </div>
+
 
 @endsection
