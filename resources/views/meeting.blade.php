ec6fff9235e23274fc6959d50ac6e1c2f34d7c7e
Michał Wybierała <mw056811@student.ath.edu.pl>
2022-02-19 16:12:49 +0100

    Added text message for empty comments

--- resources/views/meeting.blade.php
+++ resources/views/meeting.blade.php
@@ -16,24 +16,33 @@
             Komentarze
         </h3>
 
-        @foreach($meeting->comments as $comment)
-            @include('partials.comment', ['comment' => $comment])
-        @endforeach
+        @if($meeting->comments->empty())
+            <div class="mb-2" style="font-size: 18px;">
+                Na razie brak komentarzy.
+                @auth
+                    Może dodasz pierwszy komentarz?
+                @endauth
+            </div>
+        @else
+            @foreach($meeting->comments as $comment)
+                @include('partials.comment', ['comment' => $comment])
+            @endforeach
+        @endif
 
         @auth
-                <div class="row">
-                    <div class="col-12">
-                        <h3>Dodaj komentarz</h3>
-                    </div>
-                    <div class="col-12">
-                        <form method="post" action="{{ route('meeting.comments.store', [ 'meeting' => $meeting ]) }}">
-                            @csrf
+            <div class="row">
+                <div class="col-12">
+                    <h3>Dodaj komentarz</h3>
+                </div>
+                <div class="col-12">
+                    <form method="post" action="{{ route('meeting.comments.store', [ 'meeting' => $meeting ]) }}">
+                        @csrf
 
-                            <textarea class="w-100 comment__wrapper" name="content"></textarea>
+                        <textarea class="w-100 comment__wrapper" name="content"></textarea>
 
-                            <input class="mt-2" type="submit">
-                        </form>
-                    </div>
+                        <input class="mt-2" type="submit">
+                    </form>
+                </div>
             </div>
         @endauth
     </div>
