45d6258e32750ad3dfa2997c46c25195a27b4d96
Michał Wybierała <mw056811@student.ath.edu.pl>
2022-02-19 16:06:59 +0100

    Made the header width the same as the rest of the page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,20 @@
-<div class="site-header container d-flex justify-content-between">
-    <a href="/" class="text-decoration-none">
-        <div class="py-2">
-            Event.ly
-        </div>
-    </a>
-
-    <nav class="d-flex align-items-center">
-        <a href="#" class="site-nav__item position-relative">
-            <i class="fas fa-envelope"></i>
-            <span
-                class="site-nav__message-count position-absolute d-flex align-items-center justify-content-center">2</span>
+<div class="container">
+    <div class="site-header d-flex justify-content-between">
+        <a href="/" class="text-decoration-none">
+            <div class="py-2">
+                Event.ly
+            </div>
         </a>
-        <a href="#" class="site-nav__item position-relative ms-3"><i class="fas fa-cog"></i></a>
-        <a href="{{ route('my-profile') }}" class="site-nav__item position-relative ms-3"><i class="fas fa-user"></i></a>
-    </nav>
+
+        <nav class="d-flex align-items-center">
+            <a href="#" class="site-nav__item position-relative">
+                <i class="fas fa-envelope"></i>
+                <span
+                    class="site-nav__message-count position-absolute d-flex align-items-center justify-content-center">2</span>
+            </a>
+            <a href="#" class="site-nav__item position-relative ms-3"><i class="fas fa-cog"></i></a>
+            <a href="{{ route('my-profile') }}" class="site-nav__item position-relative ms-3"><i
+                    class="fas fa-user"></i></a>
+        </nav>
+    </div>
 </div>
